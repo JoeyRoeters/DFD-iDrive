b2a78da229ad65587b6b927fd60e8b829869e92e
Joey Roeters <joeyroeters@outlook.com>
2023-11-30 14:31:48 +0100

    ID-171 - Implement base for scss with helpers, utils, variables

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import sassGlobImports from 'vite-plugin-sass-glob-import';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/sass/app.scss'],
             refresh: true,
         }),
+        sassGlobImports(),
     ],
 });
