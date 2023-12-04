6ceb274ef84b1340eb73db0d3075fd96fbf102bf
Joey Roeters <joeyroeters@outlook.com>
2023-12-04 12:35:30 +0100

    ID-173 - Added bootstrap loader into vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
+import jQuery from "jquery";
+window.$ = jQuery;
+
+import {popper} from "@popperjs/core";
+window.popper = popper;
+
+import Swal from "sweetalert2";
+
 import './bootstrap';
+
+// // global import for js files in folder
+// const folders = [
+//     'utilities',
+// ];
+// for (const folder of folders) {
+//     const dir = `./${folder}/*.js`.toString();
+//     const utils = import.meta.glob(dir, { eager: true });
+//
+//     for (const path in utils) {
+//         window[path.split('/').pop().replace(/\.\w+$/, '')] = utils[path].default;
+//     }
+// }
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import sassGlobImports from 'vite-plugin-sass-glob-import';
 
+const files = [
+    'resources/css/app.css',
+    'resources/js/app.js',
+    'resources/sass/app.scss'
+];
+import path from 'path';
+
 export default defineConfig({
+    root: __dirname,
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/sass/app.scss'],
+            input: files,
             refresh: true,
         }),
         sassGlobImports(),
