c847385c3540304ec293c048065d9191056468a8
jihye2084 <jihye2084@gmail.com>
2024-01-16 02:32:57 +0900

    ID-39
    made it into responsive
    but still didn't figure out:
    1. make the size of container bigger
    2. make each box position change based on the device size
    
    Next step:
    1. Show data of recent device
    2. Show data of weekly average scores
    3. Link each pages to recent trip

--- app/UserInterface/Domain/Homepage/Resources/views/homepage.blade.php
+++ app/UserInterface/Domain/Homepage/Resources/views/homepage.blade.php
@@ -6,24 +6,53 @@
 
 @section('content')
     <div class="homepage-under-construction">
-        <div class="card_name">Current Device</div>
-        <div class="card">Device Name</div>   
+        <div class="container"> 
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4>Current Device</h4>
+                    <div class="row">
+                    <div class="cols-sm-12">
+                            <div class="card">Device name</div>
+                        </div> 
+                    </div>
+                </div>
+                <div class="col-sm-6 mt-2">
+                    <h4>Weekly Review</h4>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="review">Speed</div>
+                            <div class="review mt-1">Gear</div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="review">Brake</div>
+                            <div class="review mt-1">Steering</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 mt-2">
+                    <h4>Recent Trips</h4>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="recent">
+                                <a class="nav-link" href="#">Trip #1</a>
+                            </div>
+                            <div class="recent mt-1">
+                                <a class="nav-link" href="#">Trip #2</a>
+                            </div>
+                            <div class="recent mt-1">
+                                <a class="nav-link" href="#">Trip #3</a>
+                            </div>
+                            <div class="recent mt-1">
+                                <a class="nav-link" href="#">Trip #4</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        <div class="recent_name">Recent Trips</div>
-        <div class="recent recent1">Trip #1</div>
-        <div class="recent recent2">Trip #2</div>
-        <div class="recent recent3">Trip #3</div>
-        <div class="recent recent4">Trip #4</div>
-
-        <div class="review_name">Case Reviews</div>
-        <div class="review speed">Speed</div>
-        <div class="review brake">Brake</div>
-        <div class="review gear">Gear</div>
-        <div class="review steering">Steering</div>
-
+        
     </div>
 
     
-
-</div>
 @endsection
