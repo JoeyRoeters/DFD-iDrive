bc0ae3b0d595f15135e6ce00d7e9d1180ae7d0eb
jihye2084 <jihye2084@gmail.com>
2024-01-17 02:38:51 +0900

    ID-39

--- app/UserInterface/Domain/Homepage/Resources/views/homepage.blade.php
+++ app/UserInterface/Domain/Homepage/Resources/views/homepage.blade.php
@@ -2,6 +2,7 @@
 
 @section('head')
     @vite('app/UserInterface/Domain/Homepage/Resources/sass/_homepage.scss')
+    @vite('app/UserInterface/Domain/Homepage/Resources/sass/_recent_device.scss')
 @endsection
 
 @section('content')
@@ -9,16 +10,60 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h4>Current Device</h4>
-
-                    <div class="card">Device name</div>
-
+                    <h4>Recent Device</h4>
+                        <div id="recent-device-wrapper" class="container">
+                            <div id="recent-device-header" class="row">
+                                <div class="row">
+                                    <div class="col-md-2 col-xs-12 icon">
+                                        <i class="fa-solid fa-car"></i>
+                                    </div>
+                                    <div class="col-10 details">
+                                                <div class="row">
+                                                    <div class="col-xl-3 col-md-12 name">
+                                                        <h3>Name</h3>
+                                                        <p>Last view</p>
+                                                    </div>
+                                                    <div class="col-xl-8 col-md-12 text-xl-end text-md-start badges">
+                                                        <div class="badge">
+                                                            <div class="badge-value fs-4">
+                                                                <i class="fa-regular fa-car"></i>
+                                                                <span>Label</span>
+                                                            </div>
+                                                            <div class="badge-label fs-6">
+                                                                <span>Device type</span>
+                                                            </div>
+                                                        </div>
+                                                        <div class="badge">
+                                                            <div class="badge-value fs-4">
+                                                                <i class="fa-regular fa-road"></i>
+                                                                <span>Trips</span>
+                                                            </div>
+                                                            <div class="badge-label fs-6">
+                                                                <span>Total trips</span>
+                                                            </div>
+                                                        </div>
+                                                        <div class="badge">
+                                                            <div class="badge-value fs-4">
+                                                                <i class="fa-regular fa-gauge-simple-high"></i>
+                                                                <span>Kilometers</span>
+                                                            </div>
+                                                            <div class="badge-label fs-6">
+                                                                <span>Total KM's</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    
+                                
+                            </div>
+                        </div>
+                    
                 </div>
             </div>
-
-
-            <div class="row mt-1">
-                <div class="col-6">
+            <div class="row">
+                <div class="col-xl-6 col-md-12">
                     <h4>Weekly Review</h4>
                     <div class="row">
                         <div class="col-xl-6 col-md-12">
@@ -28,7 +73,7 @@
                             <div class="review">Gear</div>
                         </div>
                     </div>
-                    <br>
+                    
                     <div class="row">
                         <div class="col-xl-6 col-md-12">
                             <div class="review">Brake</div>
@@ -38,20 +83,20 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-6">
+                <div class="col-xl-6 col-md-12">
                     <h4>Recent Trips</h4>
                     <div class="row">
                         <div class="col-12">
                             <div class="recent">
                                 <a class="nav-link" href="#">Trip #1</a>
                             </div>
-                            <div class="recent mt-1">
+                            <div class="recent">
                                 <a class="nav-link" href="#">Trip #2</a>
                             </div>
-                            <div class="recent mt-1">
+                            <div class="recent">
                                 <a class="nav-link" href="#">Trip #3</a>
                             </div>
-                            <div class="recent mt-1">
+                            <div class="recent">
                                 <a class="nav-link" href="#">Trip #4</a>
                             </div>
                         </div>
